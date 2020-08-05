2c25edbb203d9a9e16047efc3432fd227b10ee10
cchura94 <cchura.cpc@gmail.com>
2020-08-05 18:38:45 -0400

    vuejs en laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/hola", function(){
+    return "Hola Mundo desde Laravel";
+});
+
+Route::get("/backend", function(){
+    return ["PHP", "JAVA", "PYTHON", "JAVASCRIPT", "GO"];
+});
+
+Route::get("/framework", function(){
+    return ["nombre" => "Laravel", "lenguaje" => "PHP", "plantilla" => "Blade"];
+});
+
+Route::get("/frameworks", function(){
+    $t1 = ["nombre" => "Laravel", "lenguaje" => "PHP", "plantilla" => "Blade"];
+    $t2 = ["nombre" => "Spring boot", "lenguaje" => "JAVA", "plantilla" => "Ty----"];
+    $t3 = ["nombre" => "Symfony", "lenguaje" => "PHP", "plantilla" => "Twig"];
+    $t4 = ["nombre" => "Express", "lenguaje" => "JavaScript (Node.js)", "plantilla" => ["Pug", "ejs", "Jade", "HBS"]];
+    return [$t1, $t2, $t3, $t4];
+});
+
+Route::get("/nombre/{nombre}", function($nom){
+    return ["nombre" => $nom];
+});
+
+Route::get("/actividad", function(){
+    return view("actividades");
+});
